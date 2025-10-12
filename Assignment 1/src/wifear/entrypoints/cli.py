344aec8b737d8e9baf3d194de4a6bf01353c81ac
João <joaomonteiro29@ua.pt>
2025-10-12 19:07:16 +0100

    first spimi version

--- Assignment 1/src/wifear/entrypoints/cli.py
+++ Assignment 1/src/wifear/entrypoints/cli.py
@@ -2,17 +2,24 @@ import argparse
 
 from wifear.core.limit_memory import start_memory_monitor
 from wifear.core.logger import setup_logging
+from wifear.core.spimi import SPIMIIndexer
+from wifear.core.tokenizer import PortugueseTokenizer
 
 setup_logging()
 start_memory_monitor(show_memory_updates=True)
 
 
 def main():
-    # TODO continue here
     parser = argparse.ArgumentParser(description="Wifear Indexer CLI")
     parser.add_argument("file_path", type=str, help="Path to the file to index")
 
-    _ = parser.parse_args()
+    args = parser.parse_args()
+
+    tokenizer = PortugueseTokenizer()
+    indexer = SPIMIIndexer(tokenizer)
+
+    indexer.index_documents(args.file_path)
+    indexer.merge_blocks()
 
 
 if __name__ == "__main__":
